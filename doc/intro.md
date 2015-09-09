5602ffe4dac56da9b47638745c81ed174dae2c1d
rozifus@Stone <rozifus@gmail.com>
2015-09-09 14:51:21 +1000

    created lein project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to webtorrent
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/webtorrent/core_test.clj
@@ -0,0 +1,7 @@
+(ns webtorrent.core-test
+  (:require [clojure.test :refer :all]
+            [webtorrent.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
